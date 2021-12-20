380212b684ab9257aef1787b2fda4c2f9b76d8a2
mohamed <mokeshawy@gmail.com>
2021-12-20 14:21:27 +0200

    implement logic of san QR Code

--- app/src/main/java/com/example/scanqrcode/MainActivity.kt
+++ app/src/main/java/com/example/scanqrcode/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.scanqrcode
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.scanqrcode.databinding.ActivityMainBinding
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        handleClickScanQrCode()
+    }
+
+    private fun handleClickScanQrCode() {
+        binding.startScan.setOnClickListener {
+            resultScan.launch(ScanOptions())
+        }
+    }
+
+    private val resultScan = registerForActivityResult(ScanContract()) { result: ScanIntentResult ->
+        if (result.contents == null) {
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+        }
     }
 }
