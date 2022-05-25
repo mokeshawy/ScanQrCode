b18f8b5da7e73389d2f4e2279647eba3ea14e9ab
mohamed <mokeshawy@gmail.com>
2022-05-25 12:21:41 +0200

    implement logic of san QR Code

--- app/src/main/java/com/example/scanqrcode/MainActivity.kt
+++ app/src/main/java/com/example/scanqrcode/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.scanqrcode
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.scanqrcode.databinding.ActivityMainBinding
+import com.google.zxing.*
+import com.google.zxing.common.HybridBinarizer
+import com.google.zxing.qrcode.QRCodeReader
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanIntentResult
 import com.journeyapps.barcodescanner.ScanOptions
+import java.io.ByteArrayOutputStream
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         binding.startScan.setOnClickListener {
             resultScan.launch(ScanOptions())
         }
+
     }
 
     private val resultScan = registerForActivityResult(ScanContract()) { result: ScanIntentResult ->
@@ -30,6 +36,28 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
         } else {
             Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun detectBarCode(bitmap: Bitmap): String? {
+        val stream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+        val intArray = IntArray(bitmap.width * bitmap.height)
+        bitmap.getPixels(intArray, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+        val source: LuminanceSource = RGBLuminanceSource(bitmap.width, bitmap.height, intArray)
+        val reader: Reader = QRCodeReader()
+        return try {
+            val result = reader.decode(BinaryBitmap(HybridBinarizer(source)))
+            result.text
+        } catch (e: NotFoundException) {
+            e.printStackTrace()
+            null
+        } catch (e: ChecksumException) {
+            e.printStackTrace()
+            null
+        } catch (e: FormatException) {
+            e.printStackTrace()
+            null
         }
     }
 }
